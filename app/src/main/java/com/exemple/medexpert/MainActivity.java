9324289596f08f7030f37631ce740fd4e8c01dd7
setifmohamedsaoudi <setifmohamedsaoudi@gmail.com>
2025-11-23 14:00:49 +0100

    Update MainActivity.java

--- app/src/main/java/com/exemple/medexpert/MainActivity.java
+++ app/src/main/java/com/exemple/medexpert/MainActivity.java
@@ -1,31 +1,22 @@
 package com.example.medexpert;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
-import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.view.View;
-import android.webkit.WebChromeClient;
-import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
-import android.webkit.WebViewClient;
-import android.widget.ProgressBar;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     private WebView webView;
-    private ProgressBar progressBar;
 
     @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        
         webView = findViewById(R.id.webview);
-        progressBar = findViewById(R.id.progressBar);
-
         setupWebView();
         
         // تحميل HTML من assets
@@ -37,7 +28,6 @@ public class MainActivity extends AppCompatActivity {
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         webSettings.setDomStorageEnabled(true);
-        webSettings.setDatabaseEnabled(true);
         webSettings.setAllowFileAccess(true);
         webSettings.setAllowContentAccess(true);
         webSettings.setLoadWithOverviewMode(true);
@@ -46,34 +36,6 @@ public class MainActivity extends AppCompatActivity {
         webSettings.setDisplayZoomControls(false);
         webSettings.setSupportZoom(true);
         webSettings.setDefaultTextEncodingName("utf-8");
-
-        // إعداد WebViewClient لتتبع التحميل
-        webView.setWebViewClient(new WebViewClient() {
-            @Override
-            public void onPageStarted(WebView view, String url, Bitmap favicon) {
-                progressBar.setVisibility(View.VISIBLE);
-                super.onPageStarted(view, url, favicon);
-            }
-
-            @Override
-            public void onPageFinished(WebView view, String url) {
-                progressBar.setVisibility(View.GONE);
-                super.onPageFinished(view, url);
-            }
-
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
-                return false; // السماح لـ WebView بتحميل الروابط
-            }
-        });
-
-        // إعداد WebChromeClient لتتبع التقدم
-        webView.setWebChromeClient(new WebChromeClient() {
-            @Override
-            public void onProgressChanged(WebView view, int newProgress) {
-                progressBar.setProgress(newProgress);
-            }
-        });
     }
 
     @Override
